eb42d3ac9d9198fbbef661435c3349c2018e9362
Adam Foldvari <foldadam93@gmail.com>
2019-12-03 13:36:10 +0100

    Fix api initializer

--- app/jobs/spree_shopify_importer/importers/taxon_importer_job.rb
+++ app/jobs/spree_shopify_importer/importers/taxon_importer_job.rb
@@ -2,6 +2,12 @@ module SpreeShopifyImporter
   module Importers
     class TaxonImporterJob < ::SpreeShopifyImporterJob
       def perform(resource)
+        config = Spree::Config[:shopify_current_credentials]
+
+        if config
+          ShopifyAPI::Base.api_version = config[:api_version]
+          ShopifyAPI::Base.site = "https://#{config[:api_key]}:#{config[:password]}@#{config[:shop_domain]}/admin/"
+        end
         SpreeShopifyImporter::Importers::TaxonImporter.new(resource).import!
       end
     end
