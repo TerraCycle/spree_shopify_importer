cd854b166c1ded9d6a5f3bcaa011b213e32e58a6
Erik Csonka <erik.csonka@terracycle.com>
2020-02-28 15:53:25 +0100

    Fix product import store associations/saving

--- app/services/spree_shopify_importer/data_savers/products/product_creator.rb
+++ app/services/spree_shopify_importer/data_savers/products/product_creator.rb
@@ -16,7 +16,9 @@ module SpreeShopifyImporter
         private
 
         def create_spree_product
-          Spree::Product.create!(attributes)
+          product = Spree::Product.create!(attributes)
+          product.stores << store
+          product
         end
       end
     end
